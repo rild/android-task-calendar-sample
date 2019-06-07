2c49891c44ede385f0aa35b6ceab2b9e91d6c36c
rild <rild3178ata@gmail.com>
2019-06-07 11:39:56 +0900

    update mainactivity, add dummy eventday

--- app/src/main/java/jp/naklab/assu/android/android_material_calendar_view/MainActivity.java
+++ app/src/main/java/jp/naklab/assu/android/android_material_calendar_view/MainActivity.java
@@ -4,14 +4,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import java.util.Date;
+import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
     CalendarView calendarView;
+    HashSet<Date> eventDays;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         calendarView = findViewById(R.id.calendar_view);
+
+        initEventHandler();
+        initDummyEvent();
+    }
+
+    private void initDummyEvent() {
+        eventDays = new HashSet<Date>();
+        eventDays.add(new Date());
+
+        calendarView.updateCalendar(eventDays);
+    }
+
+    private void initEventHandler() {
         calendarView.setOnDayLongClickListener(new CalendarView.OnDayLongClickListener() {
             @Override
             public void onDayLongClick(Date date) {
