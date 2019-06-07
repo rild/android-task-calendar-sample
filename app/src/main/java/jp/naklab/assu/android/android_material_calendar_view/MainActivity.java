85ef65e5c962918acde1362d7e86ed0564de42dd
rild <rild3178ata@gmail.com>
2019-06-07 11:09:59 +0900

    initial commit

---
+++ app/src/main/java/jp/naklab/assu/android/android_material_calendar_view/MainActivity.java
@@ -0,0 +1,22 @@
+package jp.naklab.assu.android.android_material_calendar_view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    CalendarView calendarView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        calendarView = findViewById(R.id.calendar_view);
+        calendarView.setEventHandler(new CalendarView.EventHandler() {
+            @Override
+            public void onDayLongPress(Date date) {
+
+            }
+        });
+    }
+}
