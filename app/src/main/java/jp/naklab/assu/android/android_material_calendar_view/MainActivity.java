12b1744a2f1ef29e8fc7f826db4e617894412869
rild <rild3178ata@gmail.com>
2019-06-08 18:09:15 +0900

    change mainactivity, add dummy event day

--- app/src/main/java/jp/naklab/assu/android/android_material_calendar_view/MainActivity.java
+++ app/src/main/java/jp/naklab/assu/android/android_material_calendar_view/MainActivity.java
@@ -2,7 +2,10 @@ package jp.naklab.assu.android.android_material_calendar_view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
 
@@ -25,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
         eventDays = new HashSet<Date>();
         eventDays.add(new Date());
 
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+            String dateInString = "05-06-2019";
+            Date date = sdf.parse(dateInString);
+            eventDays.add(date);
+        } catch (ParseException e) {
+
+        }
         calendarView.updateCalendar(eventDays);
     }
 
@@ -32,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         calendarView.setOnDayLongClickListener(new CalendarView.OnDayLongClickListener() {
             @Override
             public void onDayLongClick(Date date) {
-
+                Toast.makeText(MainActivity.this, date.toString(), Toast.LENGTH_SHORT).show();
             }
         });
     }
