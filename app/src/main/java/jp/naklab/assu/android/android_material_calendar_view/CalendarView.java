d9dcee7175eee410efde5c8edfddc9c0aa0f0fcf
rild <rild3178ata@gmail.com>
2019-07-11 21:27:26 +0900

    add weight hashmap

--- app/src/main/java/jp/naklab/assu/android/android_material_calendar_view/CalendarView.java
+++ app/src/main/java/jp/naklab/assu/android/android_material_calendar_view/CalendarView.java
@@ -5,6 +5,7 @@ import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,10 +20,10 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
 
-public class CalendarView extends LinearLayout
-{
+public class CalendarView extends LinearLayout {
     // for logging
     private static final String LOGTAG = "Calendar View";
 
@@ -47,9 +48,10 @@ public class CalendarView extends LinearLayout
     private ImageView btnNext;
     private TextView txtDate;
     private GridView grid;
+    CalendarAdapter adapter;
 
     // seasons' rainbow
-    int[] rainbow = new int[] {
+    int[] rainbow = new int[]{
             R.color.summer,
             R.color.fall,
             R.color.winter,
@@ -57,21 +59,18 @@ public class CalendarView extends LinearLayout
     };
 
     // month-season association (northern hemisphere, sorry australia :)
-    int[] monthSeason = new int[] {2, 2, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2};
+    int[] monthSeason = new int[]{2, 2, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2};
 
-    public CalendarView(Context context)
-    {
+    public CalendarView(Context context) {
         super(context);
     }
 
-    public CalendarView(Context context, AttributeSet attrs)
-    {
+    public CalendarView(Context context, AttributeSet attrs) {
         super(context, attrs);
         initControl(context, attrs);
     }
 
-    public CalendarView(Context context, AttributeSet attrs, int defStyleAttr)
-    {
+    public CalendarView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         initControl(context, attrs);
     }
@@ -79,8 +78,7 @@ public class CalendarView extends LinearLayout
     /**
      * Load control xml layout
      */
-    private void initControl(Context context, AttributeSet attrs)
-    {
+    private void initControl(Context context, AttributeSet attrs) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.control_calendar, this);
 
@@ -91,88 +89,83 @@ public class CalendarView extends LinearLayout
         updateCalendar();
     }
 
-    private void loadDateFormat(AttributeSet attrs)
-    {
+    private void loadDateFormat(AttributeSet attrs) {
         TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.CalendarView);
 
-        try
-        {
+        try {
             // try to load provided date format, and fallback to default otherwise
             dateFormat = ta.getString(R.styleable.CalendarView_dateFormat);
             if (dateFormat == null)
                 dateFormat = DATE_FORMAT;
-        }
-        finally
-        {
+        } finally {
             ta.recycle();
         }
     }
-    private void assignUiElements()
-    {
+
+    private void assignUiElements() {
         // layout is inflated, assign local variables to components
-        header = (LinearLayout)findViewById(R.id.calendar_header);
-        btnPrev = (ImageView)findViewById(R.id.calendar_prev_button);
-        btnNext = (ImageView)findViewById(R.id.calendar_next_button);
-        txtDate = (TextView)findViewById(R.id.calendar_date_display);
-        grid = (GridView)findViewById(R.id.calendar_grid);
+        header = (LinearLayout) findViewById(R.id.calendar_header);
+        btnPrev = (ImageView) findViewById(R.id.calendar_prev_button);
+        btnNext = (ImageView) findViewById(R.id.calendar_next_button);
+        txtDate = (TextView) findViewById(R.id.calendar_date_display);
+        grid = (GridView) findViewById(R.id.calendar_grid);
     }
 
-    private void assignClickHandlers()
-    {
+    private void assignClickHandlers() {
         // add one month and refresh UI
-        btnNext.setOnClickListener(new OnClickListener()
-        {
+        btnNext.setOnClickListener(new OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 currentDate.add(Calendar.MONTH, 1);
                 updateCalendar();
             }
         });
 
         // subtract one month and refresh UI
-        btnPrev.setOnClickListener(new OnClickListener()
-        {
+        btnPrev.setOnClickListener(new OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 currentDate.add(Calendar.MONTH, -1);
                 updateCalendar();
             }
         });
 
         // long-pressing a day
-        grid.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener()
-        {
+        grid.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
 
             @Override
-            public boolean onItemLongClick(AdapterView<?> view, View cell, int position, long id)
-            {
+            public boolean onItemLongClick(AdapterView<?> view, View cell, int position, long id) {
                 // handle long-press
                 if (onDayLongClickListener == null)
                     return false;
 
-                onDayLongClickListener.onDayLongClick((Date)view.getItemAtPosition(position));
+                onDayLongClickListener.onDayLongClick((Date) view.getItemAtPosition(position));
                 return true;
             }
         });
     }
 
+    // Activity から体重のデータをもらえるようにする
+    public void setWeightData(HashMap<String, Integer> hashMap) {
+        // adapter の体重データを更新する
+        adapter.setWeightData(hashMap);
+        // adapter にデータセットが変わったことを知らせる: getView を実行させる
+        adapter.notifyDataSetChanged();
+    }
+
     /**
      * Display dates correctly in grid
      */
-    public void updateCalendar()
-    {
+    public void updateCalendar() {
         updateCalendar(null);
     }
 
     /**
      * Display dates correctly in grid
      */
-    public void updateCalendar(HashSet<Date> events)
-    {
+    public void updateCalendar(HashSet<Date> events) {
         ArrayList<Date> cells = new ArrayList<>();
-        Calendar calendar = (Calendar)currentDate.clone();
+        Calendar calendar = (Calendar) currentDate.clone();
 
         // determine the cell for current month's beginning
         calendar.set(Calendar.DAY_OF_MONTH, 1);
@@ -182,14 +175,14 @@ public class CalendarView extends LinearLayout
         calendar.add(Calendar.DAY_OF_MONTH, -monthBeginningCell);
 
         // fill cells
-        while (cells.size() < DAYS_COUNT)
-        {
+        while (cells.size() < DAYS_COUNT) {
             cells.add(calendar.getTime());
             calendar.add(Calendar.DAY_OF_MONTH, 1);
         }
 
+        adapter = new CalendarAdapter(getContext(), cells, events);
         // update grid
-        grid.setAdapter(new CalendarAdapter(getContext(), cells, events));
+        grid.setAdapter(adapter);
 
         // update title
         SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
@@ -204,24 +197,45 @@ public class CalendarView extends LinearLayout
     }
 
 
-    private class CalendarAdapter extends ArrayAdapter<Date>
-    {
+    private class CalendarAdapter extends ArrayAdapter<Date> {
         // days with events
         private HashSet<Date> eventDays;
+
+        // 体重のデータを Adapter に持たせる
+        //  例:  "Thu Jul 18 21:19:30 GMT+09:00 2019" で 59 が保存される
+        //      date.toString() で、 int を保存する
+        private HashMap<String, Integer> weightData;
+        // 体重のデータがない時
+        private final int NO_WEIGHT_DATA = -1;
+
+        // 体重のデータを渡せるようにする
+
+        public void setWeightData(HashMap<String, Integer> weightData) {
+            this.weightData = weightData;
+        }
+        public int getWeight(String dateString) {
+            if (weightData == null) return NO_WEIGHT_DATA;
+            // HashMap に keyValue のデータがない時 null が帰ってくる
+            if (weightData.get(dateString) == null) return NO_WEIGHT_DATA;
+
+            // 上の２つの場合以外は weightData から 日付で体重を 取り出せる
+            return weightData.get(dateString);
+        }
 
         // for view inflation
         private LayoutInflater inflater;
 
-        public CalendarAdapter(Context context, ArrayList<Date> days, HashSet<Date> eventDays)
-        {
+        public CalendarAdapter(Context context, ArrayList<Date> days, HashSet<Date> eventDays) {
             super(context, R.layout.control_calendar_day, days);
             this.eventDays = eventDays;
             inflater = LayoutInflater.from(context);
         }
 
         @Override
-        public View getView(int position, View view, ViewGroup parent)
-        {
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder viewHolder;
+
             // day in question
             Date date = getItem(position);
             int day = date.getDate();
@@ -232,54 +246,77 @@ public class CalendarView extends LinearLayout
             Date today = new Date();
 
             // inflate item if it does not exist yet
-            if (view == null)
-                view = inflater.inflate(R.layout.control_calendar_day, parent, false);
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.control_calendar_day, parent, false);
+                viewHolder = new ViewHolder(convertView);
+                convertView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
 
             // if this day has an event, specify event image
-            view.setBackgroundResource(0);
-            if (eventDays != null)
-            {
-                for (Date eventDate : eventDays)
-                {
-                    if (eventDate.getDate() == day &&
-                            eventDate.getMonth() == month &&
-                            eventDate.getYear() == year)
-                    {
-                        // mark this day for event
-                        view.setBackgroundResource(R.drawable.reminder);
-                        break;
-                    }
-                }
-            }
+//            convertView.setBackgroundResource(0);
+//            if (eventDays != null) {
+//                for (Date eventDate : eventDays) {
+//                    if (eventDate.getDate() == day &&
+//                            eventDate.getMonth() == month &&
+//                            eventDate.getYear() == year) {
+//                        // mark this day for event
+////                        convertView.setBackgroundResource(R.drawable.reminder);
+//                        viewHolder.imageViewEventTag.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+//                        break;
+//                    }
+//                }
+//            }
+
 
             // clear styling
-            ((TextView)view).setTypeface(null, Typeface.NORMAL);
-            ((TextView)view).setTextColor(Color.BLACK);
+            viewHolder.textViewDay.setTypeface(null, Typeface.NORMAL);
+            viewHolder.textViewDay.setTextColor(Color.BLACK);
 
-            if (month != today.getMonth() || year != today.getYear())
-            {
+            if (month != today.getMonth() || year != today.getYear()) {
                 // if this day is outside current month, grey it out
-                ((TextView)view).setTextColor(getResources().getColor(R.color.greyed_out));
-            }
-            else if (day == today.getDate())
-            {
+                viewHolder.textViewDay.setTextColor(getResources().getColor(R.color.greyed_out));
+            } else if (day == today.getDate()) {
                 // if it is today, set it to blue/bold
-                ((TextView)view).setTypeface(null, Typeface.BOLD);
-                ((TextView)view).setTextColor(getResources().getColor(R.color.today));
+                viewHolder.textViewDay.setTypeface(null, Typeface.BOLD);
+                viewHolder.textViewDay.setTextColor(getResources().getColor(R.color.today));
+            }
+            // set text
+            viewHolder.textViewDay.setText(String.valueOf(date.getDate()));
+
+
+            // カレンダーに体重を表示させる
+            int weight = adapter.getWeight(date.toString());
+            if (weight != NO_WEIGHT_DATA) {
+                String weightString = String.valueOf(weight);
+                viewHolder.textViewWeight.setText(weightString);
+
+                // 体重が入力された日にタグをつける
+                viewHolder.imageViewEventTag.setBackgroundColor(getResources().getColor(R.color.colorAccent));
             }
 
-            // set text
-            ((TextView)view).setText(String.valueOf(date.getDate()));
 
-            return view;
+            return convertView;
+        }
+
+        class ViewHolder {
+            TextView textViewDay;
+            ImageView imageViewEventTag;
+            TextView textViewWeight;
+
+            ViewHolder(View v) {
+                textViewDay = v.findViewById(R.id.textview_day);
+                imageViewEventTag = v.findViewById(R.id.imageview_eventtag);
+                textViewWeight = v.findViewById(R.id.textview_weight);
+            }
         }
     }
 
     /**
      * Assign event handler to be passed needed events
      */
-    public void setOnDayLongClickListener(OnDayLongClickListener l)
-    {
+    public void setOnDayLongClickListener(OnDayLongClickListener l) {
         this.onDayLongClickListener = l;
     }
 
@@ -287,8 +324,7 @@ public class CalendarView extends LinearLayout
      * This interface defines what events to be reported to
      * the outside world
      */
-    public interface OnDayLongClickListener
-    {
+    public interface OnDayLongClickListener {
         void onDayLongClick(Date date);
     }
 }
--- app/src/main/java/jp/naklab/assu/android/android_material_calendar_view/MainActivity.java
+++ app/src/main/java/jp/naklab/assu/android/android_material_calendar_view/MainActivity.java
@@ -1,17 +1,28 @@
 package jp.naklab.assu.android.android_material_calendar_view;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
     CalendarView calendarView;
     HashSet<Date> eventDays;
+
+    // 体重のデータ
+    // TODO あとで保存する必要がある
+    HashMap<String, Integer> hashMap = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +55,39 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDayLongClick(Date date) {
                 Toast.makeText(MainActivity.this, date.toString(), Toast.LENGTH_SHORT).show();
+                // 日付が長押しされたら入力ダイアログを開く
+                createInputDialog(date);
             }
         });
+    }
+
+
+    // 入力するダイアログの表示
+    AlertDialog mDialog;
+    private void createInputDialog(Date date) {
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate( R.layout.input_dialog, null );
+
+        EditText weightInput = (EditText) view.findViewById(R.id.edittext_input);
+
+        Button btnPositive = (Button) view.findViewById(R.id.button_dialog_positive);
+        btnPositive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // EditText から体重のデータを int 型に変換して hashMap に保存する
+                // その時の鍵 (keyValue) は String型の日付
+                int weight = Integer.parseInt(weightInput.getText().toString());
+                hashMap.put(date.toString(), weight);
+                calendarView.setWeightData(hashMap);
+
+                // ダイアログを閉じる
+                mDialog.dismiss();
+            }
+        });
+
+        mDialog = new AlertDialog.Builder(MainActivity.this)
+                .setView( view )
+                .create();
+        mDialog.show();
     }
 }
